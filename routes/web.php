61f90bc0462e4e7840dd5915a9c72792ccf1f1bb
Salim66 <salimhasanriad8@gmail.com>
2021-08-20 08:27:03 +0600

    Updating data

--- routes/web.php
+++ routes/web.php
@@ -35,3 +35,12 @@ Route::get('/insert', function(){
     $user->address()->save($address);
 
 });
+
+// update data
+Route::get('/update', function(){
+
+    $address = Address::whereUserId(1)->first();
+    $address->name = "Monipur 243 Dhaka";
+    $address->update();
+
+});
